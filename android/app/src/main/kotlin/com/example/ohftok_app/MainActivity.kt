e324ff352e6d1210d3ecdffe8c43296f9df3e0a7
Roger <roger.b.hunt.iii@gauntletai.com>
2025-02-13 17:39:45 -0600

    Clean repository and add only essential files

--- android/app/src/main/kotlin/com/example/ohftok_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ohftok_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/ohftok5/flutter/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package ohftok5.flutter.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-} 
